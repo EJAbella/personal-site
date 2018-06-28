2b11d18cb32e9d1f6e88dd1b8645de489f606e4b
Elijah <elijah.abella@aol.com>
2018-06-28 14:49:29 -0700

    Draft w/ all pages complete

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-$('.works').mouseenter(function() {
-    $(this).css("cursor","pointer");
-    $(this).animate({height: 700;}, 'slow');
-});
-
-$('.works').mouseleave(function() {
-    $(this).animate({width: "28%"}, 'slow');
+document.getElementById('work1').addEventListener('click', function() {
+  work1.style.color = 'blue'
 });
